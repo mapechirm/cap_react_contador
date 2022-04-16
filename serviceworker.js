8b809caa1b27f09f2ac3a3db72bb491961a01373
Antonio Pechir <mapechirm@hotmail.com>
2022-04-15 19:06:16 -0500

    Updates

---
+++ serviceworker.js
@@ -0,0 +1,36 @@
+const CACHE_ELEMENTS = ["./"];
+
+const CACHE_NAME = "v1_cache_contador_react";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache
+        .addAll(CACHE_ELEMENTS)
+        .then(() => self.skipWaiting())
+        .catch((err) => console.log(err));
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const cacheWhiteList = [CACHE_NAME];
+
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return (
+            cacheWhiteList.indexOf(cacheName) === -1 && caches.delete(cacheName)
+          );
+        })
+      );
+    })
+  ).then(() => self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => (res ? res : fetch(e.request)))
+  );
+});
